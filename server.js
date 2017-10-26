1c47f24d7c90962aa5ff53d335bd60597c8e9141
Suyog Wani <suyog.wani@marktplatz-mittelstand.de>
2017-10-26 13:28:25 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`
+
+  console.log(`${now}: ${req.method} ${req.url}`);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err) {
+      console.log(err);
+    }
+  });
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render('maintenance.hbs', {
+    pageHead: 'Maintenance under way!',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/', (req, res) => {
+  res.render('welcome.hbs', {
+    pageHead: 'Welcome to Springfield',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs',{
+    pageHead: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: "Bad REquest"
+  });
+});
+
+app.listen(3000);
